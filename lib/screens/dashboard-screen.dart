31bab7dfbc272b96664198332ba59a87cb06df47
navin1994 <navin.yele@gmail.com>
2021-05-30 23:28:11 +0530

    Added raise complaint button on dashboard

--- lib/screens/dashboard-screen.dart
+++ lib/screens/dashboard-screen.dart
@@ -7,7 +7,7 @@ import '../translations/locale_keys.g.dart';
 import '../providers/complaints.dart';
 import '../widgets/app_drawer.dart';
 import '../widgets/tile_widget.dart';
-// import '../widgets/pending_complaint_card.dart';
+import '../screens/raise_complain_screen.dart';
 import '../screens/complaint_management_screen.dart';
 
 class Dashboard extends StatelessWidget {
@@ -56,123 +56,160 @@ class Dashboard extends StatelessWidget {
             : Consumer<Complaints>(
                 builder: (ctx, cmpl, _) => SafeArea(
                   child: Container(
-                    padding: EdgeInsets.all(10),
-                    child: cmpl.complaintSummary.isEmpty
-                        ? Center(
-                            child: Text(
-                                "${LocaleKeys.error_dashboard_summary.tr()}",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    fontSize: 22, color: Colors.white)))
-                        : ListView(
-                            children: [
-                              // Text('Pending Complaints',
-                              //     style: TextStyle(fontSize: 22, color: Colors.white)),
-                              // SizedBox(height: 10),
-                              // PendingComplaintCard(),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  Text(LocaleKeys.cmpl_data.tr(),
+                    width: double.infinity,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          height: MediaQuery.of(context).size.height * .80,
+                          padding: EdgeInsets.all(10),
+                          child: cmpl.complaintSummary.isEmpty
+                              ? Center(
+                                  child: Text(
+                                      "${LocaleKeys.error_dashboard_summary.tr()}",
+                                      textAlign: TextAlign.center,
                                       style: TextStyle(
-                                          fontSize: 22, color: Colors.white)),
-                                  InkWell(
-                                    child: Text(
-                                        '${LocaleKeys.view_all_.tr()} >>',
-                                        style: TextStyle(
-                                            fontSize: 15, color: Colors.white)),
-                                    onTap: () {
-                                      Navigator.of(context)
-                                          .pushReplacementNamed(
-                                              ComplaintManagementScreen
-                                                  .routeName);
-                                    },
-                                  ),
-                                ],
-                              ),
-                              SizedBox(height: 15),
-                              InkWell(
-                                onTap: () {
-                                  Navigator.of(context).pushReplacementNamed(
-                                      ComplaintManagementScreen.routeName,
-                                      arguments: 5);
-                                },
-                                child: Tilewidget(
-                                  color: Colors.yellow,
-                                  symbol: cmpl.complaintSummary[0].text,
-                                  icon:
-                                      Icon(Icons.calculate_outlined, size: 32),
-                                  perc: 100.0,
-                                  textcolor: Colors.purpleAccent,
-                                  count:
-                                      int.parse(cmpl.complaintSummary[0].value),
+                                          fontSize: 22, color: Colors.white)))
+                              : ListView(
+                                  children: [
+                                    // Text('Pending Complaints',
+                                    //     style: TextStyle(fontSize: 22, color: Colors.white)),
+                                    // SizedBox(height: 10),
+                                    // PendingComplaintCard(),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceAround,
+                                      children: [
+                                        Text(LocaleKeys.cmpl_data.tr(),
+                                            style: TextStyle(
+                                                fontSize: 22,
+                                                color: Colors.white)),
+                                        InkWell(
+                                          child: Text(
+                                              '${LocaleKeys.view_all_.tr()} >>',
+                                              style: TextStyle(
+                                                  fontSize: 15,
+                                                  color: Colors.white)),
+                                          onTap: () {
+                                            Navigator.of(context)
+                                                .pushReplacementNamed(
+                                                    ComplaintManagementScreen
+                                                        .routeName);
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(height: 15),
+                                    InkWell(
+                                      onTap: () {
+                                        Navigator.of(context)
+                                            .pushReplacementNamed(
+                                                ComplaintManagementScreen
+                                                    .routeName,
+                                                arguments: 5);
+                                      },
+                                      child: Tilewidget(
+                                        color: Colors.yellow,
+                                        symbol: cmpl.complaintSummary[0].text,
+                                        icon: Icon(Icons.calculate_outlined,
+                                            size: 32),
+                                        perc: 100.0,
+                                        textcolor: Colors.purpleAccent,
+                                        count: int.parse(
+                                            cmpl.complaintSummary[0].value),
+                                      ),
+                                    ),
+                                    InkWell(
+                                      onTap: () {
+                                        Navigator.of(context)
+                                            .pushReplacementNamed(
+                                                ComplaintManagementScreen
+                                                    .routeName,
+                                                arguments: 3);
+                                      },
+                                      child: Tilewidget(
+                                        color: Colors.greenAccent,
+                                        symbol: cmpl.complaintSummary[1].text,
+                                        icon: Icon(Icons.check_circle_outline,
+                                            size: 32),
+                                        perc: ((double.parse(cmpl
+                                                    .complaintSummary[1]
+                                                    .value) *
+                                                100) /
+                                            double.parse(cmpl
+                                                .complaintSummary[0].value)),
+                                        textcolor: Colors.greenAccent,
+                                        count: int.parse(
+                                            cmpl.complaintSummary[1].value),
+                                      ),
+                                    ),
+                                    InkWell(
+                                      onTap: () {
+                                        Navigator.of(context)
+                                            .pushReplacementNamed(
+                                                ComplaintManagementScreen
+                                                    .routeName,
+                                                arguments: 1);
+                                      },
+                                      child: Tilewidget(
+                                        color: Colors.orange,
+                                        symbol: cmpl.complaintSummary[3].text,
+                                        icon: Icon(Icons.pending_actions,
+                                            size: 32),
+                                        perc: ((double.parse(cmpl
+                                                    .complaintSummary[3]
+                                                    .value) *
+                                                100) /
+                                            double.parse(cmpl
+                                                .complaintSummary[0].value)),
+                                        textcolor: Colors.amberAccent,
+                                        count: int.parse(
+                                            cmpl.complaintSummary[3].value),
+                                      ),
+                                    ),
+                                    InkWell(
+                                      onTap: () {
+                                        Navigator.of(context)
+                                            .pushReplacementNamed(
+                                                ComplaintManagementScreen
+                                                    .routeName,
+                                                arguments: 4);
+                                      },
+                                      child: Tilewidget(
+                                        color: Colors.red,
+                                        symbol: cmpl.complaintSummary[2].text,
+                                        icon: Icon(
+                                            Icons.highlight_remove_outlined,
+                                            size: 32),
+                                        perc: ((double.parse(cmpl
+                                                    .complaintSummary[2]
+                                                    .value) *
+                                                100) /
+                                            double.parse(cmpl
+                                                .complaintSummary[0].value)),
+                                        textcolor: Colors.redAccent,
+                                        count: int.parse(
+                                            cmpl.complaintSummary[2].value),
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ),
-                              InkWell(
-                                onTap: () {
-                                  Navigator.of(context).pushReplacementNamed(
-                                      ComplaintManagementScreen.routeName,
-                                      arguments: 3);
-                                },
-                                child: Tilewidget(
-                                  color: Colors.greenAccent,
-                                  symbol: cmpl.complaintSummary[1].text,
-                                  icon: Icon(Icons.check_circle_outline,
-                                      size: 32),
-                                  perc: ((double.parse(
-                                              cmpl.complaintSummary[1].value) *
-                                          100) /
-                                      double.parse(
-                                          cmpl.complaintSummary[0].value)),
-                                  textcolor: Colors.greenAccent,
-                                  count:
-                                      int.parse(cmpl.complaintSummary[1].value),
-                                ),
-                              ),
-                              InkWell(
-                                onTap: () {
-                                  Navigator.of(context).pushReplacementNamed(
-                                      ComplaintManagementScreen.routeName,
-                                      arguments: 1);
-                                },
-                                child: Tilewidget(
-                                  color: Colors.orange,
-                                  symbol: cmpl.complaintSummary[3].text,
-                                  icon: Icon(Icons.pending_actions, size: 32),
-                                  perc: ((double.parse(
-                                              cmpl.complaintSummary[3].value) *
-                                          100) /
-                                      double.parse(
-                                          cmpl.complaintSummary[0].value)),
-                                  textcolor: Colors.amberAccent,
-                                  count:
-                                      int.parse(cmpl.complaintSummary[3].value),
-                                ),
-                              ),
-                              InkWell(
-                                onTap: () {
-                                  Navigator.of(context).pushReplacementNamed(
-                                      ComplaintManagementScreen.routeName,
-                                      arguments: 4);
-                                },
-                                child: Tilewidget(
-                                  color: Colors.red,
-                                  symbol: cmpl.complaintSummary[2].text,
-                                  icon: Icon(Icons.highlight_remove_outlined,
-                                      size: 32),
-                                  perc: ((double.parse(
-                                              cmpl.complaintSummary[2].value) *
-                                          100) /
-                                      double.parse(
-                                          cmpl.complaintSummary[0].value)),
-                                  textcolor: Colors.redAccent,
-                                  count:
-                                      int.parse(cmpl.complaintSummary[2].value),
-                                ),
-                              )
-                            ],
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              textStyle: const TextStyle(fontSize: 20),
+                              primary: Colors.blue, // background
+                              onPrimary: Colors.white, // foreground
+                            ),
+                            onPressed: () => Navigator.of(context)
+                                .pushNamed(RaiseComplainScreen.routeName),
+                            child: Text("Raise Complaint"),
                           ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
