f75fa2cb31db38eef15e570a80777e630ced0938
navin1994 <navin.yele@gmail.com>
2021-07-08 00:03:24 +0530

    Chnaged UAT api url

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -1,5 +1,6 @@
 class Environment {
-  static const url = "http://forcmplnt.sparsht.com/SPComplaint";
-  // static const url = "http://forcmplnt.sparsht.com/uatcmplnt"; // For testing
+  // static const url = "http://forcmplnt.sparsht.com/SPComplaint";
+  static const url =
+      "http://uatforcmplnt.sparsht.com/SPComplaint"; // For testing
   // static const url = "http://192.168.43.128:3000";
 }
