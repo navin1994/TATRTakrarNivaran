fe9f74d28bf3c19d45d507dd0c41c4e39a8601fd
navin1994 <navin.yele@gmail.com>
2021-08-01 14:21:35 +0530

    Chnaged production API URL

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -1,5 +1,5 @@
 class Environment {
-  static const url = "http://forcmplnt.sparsht.com/SPComplaint";
+  static const url = "http://forcmplnt.sparsht.com/SPComplaint.2.2.0";
   // static const url =
   //     "http://uatforcmplnt.sparsht.com/SPComplaint"; // For testing
   // static const url = "http://192.168.43.128:3000";
